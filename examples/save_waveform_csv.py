06484fe3aa45a7da918851f2333d7f38ade2e91e
Aleksandr Chasnyk <69671996+ami3go@users.noreply.github.com>
2026-08-19 11:42:53 +0300

    Normalize examples

---
+++ examples/save_waveform_csv.py
@@ -0,0 +1,10 @@
+"""Save enabled channels to one CSV file."""
+
+from dpo4000_utils import DPO4054
+
+
+scope = DPO4054(auto_connect=True)
+try:
+    scope.save_all_channels_to_single_csv("waveform_enabled_channels.csv")
+finally:
+    scope.disconnect()
