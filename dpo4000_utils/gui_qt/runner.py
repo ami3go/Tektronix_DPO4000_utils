1783497d8f518139a1df82c6b5c490475aafd0c8
Aleksandr Chasnyk <69671996+ami3go@users.noreply.github.com>
2026-08-20 23:38:47 +0300

    Launch Qt acquisition tab window

--- dpo4000_utils/gui_qt/runner.py
+++ dpo4000_utils/gui_qt/runner.py
@@ -17,7 +17,7 @@ def main() -> int:
             "  python -m pip install -r requirements-pyside6.txt\n"
         ) from exc
 
-    from .ui_practice_window import QtScopeWindow
+    from .acquisition_window import QtScopeWindow
 
     app = QApplication(sys.argv)
     window = QtScopeWindow()
